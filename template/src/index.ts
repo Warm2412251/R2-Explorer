942e218bc31334a15f351ae593eec6cc2c5c91ba
Camel <162074747+Warm2412251@users.noreply.github.com>
2026-04-30 09:05:30 +0800

    Refactor R2Explorer export for better readability

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,6 +1,9 @@
 import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({ readonly: false, basicAuth: {
+export default R2Explorer({
+  readonly: false,
+
+  basicAuth: {
     username: 'password',
     password: 'Wasd&5998'
   }
